bd1022508ab2beeeb8fd9140c6ed75feac7be3f3
JavierBailen <javier.bailen@fernando3martos.com>
2025-03-06 12:18:51 +0100

    update

--- src/app/services/mgs-service.service.ts
+++ src/app/services/mgs-service.service.ts
@@ -8,7 +8,7 @@ import { MgsPersonaje } from '../interfaces/mgs-personaje';
 })
 export class MgsServiceService {
 
-  private apiUrl = 'https://backend-proyecto-mgs.onrender.com/api/personajes';
+  private apiUrl = 'https://backend-proyecto-mgs.onrender.com';
 
   constructor(private http: HttpClient) {
 
